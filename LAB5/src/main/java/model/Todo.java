0a3672965dd8bc77764049bdfe02947245ff655f
tomas <tomas.suarez@mail.escuelaing.edu.co>
2024-02-23 20:04:02 -0500

    Ejercicio 3

---
+++ LAB5/src/main/java/model/Todo.java
@@ -0,0 +1,44 @@
+package model;
+
+public class Todo {
+    private int userId;
+    private int id;
+    private String title;
+    private boolean completed;
+
+    public Todo() {
+    }
+
+    // Getters y setters para los campos
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
