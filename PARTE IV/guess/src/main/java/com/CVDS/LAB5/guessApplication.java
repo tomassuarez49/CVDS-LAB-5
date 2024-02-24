12c1fdb2db3b1865c693e6774bdaedfe03ff9467
Nicolas <nicolas.achuri-m@mail.escuelaing.edu.co>
2024-02-23 20:47:00 -0500

    Casi terminado

---
+++ PARTE IV/guess/src/main/java/com/CVDS/LAB5/guessApplication.java
@@ -0,0 +1,13 @@
+package com.CVDS.LAB5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class guessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(guessApplication.class, args);
+	}
+
+}
